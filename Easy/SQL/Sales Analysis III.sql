10b7f548c35d05d4fc647094e7bf064ae893f528
Alexandra Dumitrescu <165086014+adumitrescu1207@users.noreply.github.com>
2025-09-19 12:10:58 +0300

    Create Sales Analysis III.sql

---
+++ Easy/SQL/Sales Analysis III.sql
@@ -0,0 +1,41 @@
+/*
+Table: Product
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| product_id   | int     |
+| product_name | varchar |
+| unit_price   | int     |
++--------------+---------+
+product_id is the primary key (column with unique values) of this table.
+Each row of this table indicates the name and the price of each product.
+Table: Sales
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| seller_id   | int     |
+| product_id  | int     |
+| buyer_id    | int     |
+| sale_date   | date    |
+| quantity    | int     |
+| price       | int     |
++-------------+---------+
+This table can have duplicate rows.
+product_id is a foreign key (reference column) to the Product table.
+Each row of this table contains some information about one sale.
+ 
+
+Write a solution to report the products that were only sold in the first quarter of 2019. That is, between 2019-01-01 and 2019-03-31 inclusive.
+*/
+select distinct p.product_id, p.product_name
+from Product as p
+join Sales as s
+on p.product_id = s.product_id
+where s.sale_date between '2019-01-01' and '2019-03-31'
+and not exists 
+(select * 
+from Sales as ss
+where ss.product_id = p.product_id 
+and (ss.sale_date < '2019-01-01' or ss.sale_date > '2019-03-31'));
