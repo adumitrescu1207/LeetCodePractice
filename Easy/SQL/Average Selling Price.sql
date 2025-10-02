b85df8ca386b3e41b635d78623621d9beed00530
Alexandra Dumitrescu <165086014+adumitrescu1207@users.noreply.github.com>
2025-10-02 11:37:05 +0300

    Create Average Selling Price.sql

---
+++ Easy/SQL/Average Selling Price.sql
@@ -0,0 +1,40 @@
+/*
+Table: Prices
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| product_id    | int     |
+| start_date    | date    |
+| end_date      | date    |
+| price         | int     |
++---------------+---------+
+(product_id, start_date, end_date) is the primary key (combination of columns with unique values) for this table.
+Each row of this table indicates the price of the product_id in the period from start_date to end_date.
+For each product_id there will be no two overlapping periods. That means there will be no two intersecting periods for the same product_id.
+ 
+
+Table: UnitsSold
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| product_id    | int     |
+| purchase_date | date    |
+| units         | int     |
++---------------+---------+
+This table may contain duplicate rows.
+Each row of this table indicates the date, units, and product_id of each product sold. 
+ 
+
+Write a solution to find the average selling price for each product. average_price should be rounded to 2 decimal places. If a product does not have any sold units, its average selling price is assumed to be 0.
+
+Return the result table in any order.
+*/
+select p.product_id,
+round(sum(us.units*p.price)/sum(us.units), 2) as average_price
+from Prices as p
+join UnitsSold as us
+on p.product_id = us.product_id
+and us.purchase_date between p.start_date and p.end_date
+group by p.product_id;
